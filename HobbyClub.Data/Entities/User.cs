8e1547e76cac78c1e6695bf5dfc726f8ad264e00
Andrii_Vasylyshyn <andrew.vas.o2008@gmail.com>
2015-03-15 21:52:02 +0200

    User entity added

---
+++ HobbyClub.Data/Entities/User.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HobbyClub.Data.Entities
+{
+    class User
+    {
+        public UInt32 UserID { get; set; }
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public string Mail { get; set; }
+        public string Phone { get; set; }
+        public string City { get; set; }
+        public string Country { get; set; }
+        public virtual UInt32 PhotoID { get; set; }
+        public DateTime CreationDate { get; set; }
+    }
+}
