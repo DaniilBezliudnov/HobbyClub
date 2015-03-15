6cd7ca91bd04387af74ff2c4465006e37987ecab
Andrii_Vasylyshyn <andrew.vas.o2008@gmail.com>
2015-03-15 22:02:47 +0200

    Group entity added

---
+++ HobbyClub.Data/Entities/Group.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HobbyClub.Data.Entities
+{
+    class Group
+    {
+        public UInt32 GroupID { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public DateTime CreationDate { get; set; }
+        public virtual UInt32 LogoID { get; set; }
+    }
+}
