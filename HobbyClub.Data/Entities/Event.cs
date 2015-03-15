6992ca52302a15a3b37587a21f64585aeb2e22c9
Andrii_Vasylyshyn <andrew.vas.o2008@gmail.com>
2015-03-15 22:17:36 +0200

    Event entity added without Comment field

---
+++ HobbyClub.Data/Entities/Event.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HobbyClub.Data.Entities
+{
+    class Event
+    {
+        public UInt32 EventID { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public EventType EventType  { get; set; }
+        public DateTime CreationDate { get; set; }
+        public virtual UInt32 LogoID { get; set; }
+        public virtual UInt32 InterestID { get; set; }
+        public virtual UInt32 GroupID { get; set; }
+        public virtual UInt32 CreatorID { get; set; }
+        
+    }
+
+    enum EventType
+    {
+        Public,
+        Private
+    }
+}
