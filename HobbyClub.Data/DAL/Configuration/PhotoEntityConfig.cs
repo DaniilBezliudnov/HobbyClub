ca3705cebc3840865ab1e099a4bd484abc6301ba
Andrii_Vasylyshyn <andrew.vas.o2008@gmail.com>
2015-03-16 01:52:11 +0200

    Photo entity config added;

---
+++ HobbyClub.Data/DAL/Configuration/PhotoEntityConfig.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.Entity.ModelConfiguration;
+using HobbyClub.Data.Entities;
+
+namespace HobbyClub.Data.DAL.Configuration
+{
+    class PhotoEntityConfig : EntityTypeConfiguration<Photo>
+    {
+        public PhotoEntityConfig()
+        {
+            this.HasKey(p => p.PhotoId);
+            this.Property(p => p.Value).IsRequired();
+        }
+    }
+}
