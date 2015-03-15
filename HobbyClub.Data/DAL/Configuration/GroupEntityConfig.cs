0f42e8d498f6a1e5bf3647079cbee6ecc51cd657
Andrii_Vasylyshyn <andrew.vas.o2008@gmail.com>
2015-03-16 01:47:39 +0200

    Group entity config added

---
+++ HobbyClub.Data/DAL/Configuration/GroupEntityConfig.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.Entity.ModelConfiguration;
+using HobbyClub.Data.Entities;
+
+namespace HobbyClub.Data.DAL.Configuration
+{
+    class GroupEntityConfig : EntityTypeConfiguration<Group>
+    {
+        public GroupEntityConfig()
+        {
+            this.ToTable("Group");
+            this.HasKey<UInt32>(gr => gr.GroupId);
+            this.Property(p => p.Name).IsRequired();
+            this.Property(p => p.Description).IsRequired();
+            this.HasOptional(p => p.Users);
+            this.HasOptional(p => p.LogoID)
+                .WithRequired(g => g.Group);
+        }
+    }
+}
