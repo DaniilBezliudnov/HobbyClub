a3925560a4936587ba6abaecb0649f2f409765bc
alexakryvenko <alexakryvenko@gmail.com>
2015-03-16 13:28:15 +0200

    Base authentication

---
+++ HobbyClub.Data/Entities/AppRole.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNet.Identity.EntityFramework;
+namespace HobbyClub.Data.Entities
+{
+    public class AppRole : IdentityRole
+    {
+        public AppRole() : base() { }
+        public AppRole(string name) : base(name) { }
+    }
+}
---
+++ HobbyClub.Data/Infrastructure/AppRoleManager.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+using Microsoft.AspNet.Identity.Owin;
+using Microsoft.Owin;
+using HobbyClub.Data.Entities;
+namespace HobbyClub.Data.Infrastructure
+{
+    public class AppRoleManager : RoleManager<AppRole>, IDisposable
+    {
+        public AppRoleManager(RoleStore<AppRole> store) : base(store)
+        {
+        }
+        public static AppRoleManager Create(IdentityFactoryOptions<AppRoleManager> options, IOwinContext context)
+        {
+            return new AppRoleManager(new RoleStore<AppRole>(context.Get<HobbyClubIdentityDbContext>()));
+        }
+       
+    }
+}
