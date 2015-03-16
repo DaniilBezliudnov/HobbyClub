3ab86a31e576558ae55b9e9857b5daf781a6cf86
Andrii_Vasylyshyn <andrew.vas.o2008@gmail.com>
2015-03-16 13:39:58 +0200

    EntityDataBaseFactory added

---
+++ HobbyClub.Data/Concrete/EntityDataBaseFactory.cs
@@ -0,0 +1,30 @@
+﻿using HobbyClub.Data.Infrastructure;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.Entity;
+
+namespace HobbyClub.Data.Concrete
+{
+    class EntityDataBaseFactory : IDataBaseFactory, IDisposable
+    {
+        private DbContext context;
+        public EntityDataBaseFactory(DbContext context)
+        {
+            this.context = context;
+        }
+        public DbContext Get()
+        {
+            if (context != null)
+                return context;
+            else return null;
+        }
+        public void Dispose()
+        {
+            if (context != null)
+                context.Dispose();
+        }
+    }
+}
