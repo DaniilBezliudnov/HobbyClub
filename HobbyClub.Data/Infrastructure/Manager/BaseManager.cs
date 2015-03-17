cfe8f93033e7f52248e98949b1f1558e29599193
Andrii_Vasylyshyn <andrew.vas.o2008@gmail.com>
2015-03-17 10:59:08 +0200

    base manager added

---
+++ HobbyClub.Data/Infrastructure/Manager/BaseManager.cs
@@ -0,0 +1,28 @@
+﻿using HobbyClub.Data.Concrete;
+using HobbyClub.Data.Infrastructure.Context;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HobbyClub.Data.Infrastructure.Manager
+{
+    class BaseManager
+    {
+        private IDataBaseFactory dbContext;
+
+        public BaseManager()
+        {
+
+        }
+
+        protected IDataBaseFactory Get()
+        {
+            if (dbContext == null)
+                dbContext = new EntityDataBaseFactory(new HobbyClubContext());
+            return this.dbContext;
+        }
+    }
+}
