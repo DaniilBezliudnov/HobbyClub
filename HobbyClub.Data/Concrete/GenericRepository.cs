ee15e74510d3593786da1fa578f8ea4c3b6d6563
Andrii_Vasylyshyn <andrew.vas.o2008@gmail.com>
2015-03-25 16:13:07 +0200

    Delete factory methods; Change Repository Class to GenericRepository

---
+++ HobbyClub.Data/Concrete/GenericRepository.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+using HobbyClub.Data.Abstract;
+
+namespace HobbyClub.Data.Concrete
+{
+    public abstract class GenericRepository<TEntity, TContext> :
+        IBaseRepository<TEntity>
+        where TEntity : class
+        where TContext : DbContext, new()
+    {
+        private readonly TContext _context = new TContext();
+
+        public DbSet<TEntity> Table
+        {
+            get { return _context.Set<TEntity>(); }
+        }
+
+        public virtual void Add(TEntity entity)
+        {
+            if (entity != null)
+            {
+                this.Table.Add(entity);
+            }
+        }
+        public virtual void Delete(TEntity entity)
+        {
+            if (entity != null)
+            {
+                this.Table.Remove(entity);
+            }
+        }
+        public virtual TEntity Get(Expression<Func<TEntity, bool>> where)
+        {
+            return this.Table.Where(where).FirstOrDefault();
+        }
+        public virtual void Update(TEntity entity)
+        {
+            if (entity != null)
+            {
+                this.Table.Attach(entity);
+                this._context.Entry(entity).State = EntityState.Modified;
+            }
+        }
+        public virtual IQueryable<TEntity> GetAll()
+        {
+            return this.Table;
+        }
+        public virtual IQueryable<TEntity> GetMany(Expression<Func<TEntity, bool>> where)
+        {
+            return this.Table.Where(where);
+        }
+
+        public virtual void Commit()
+        {
+            if (this._context == null)
+                throw new ArgumentNullException("Null reference Dbcontext!");
+
+            using (var scope = this._context.Database.BeginTransaction())
+            {
+                this._context.SaveChanges();
+                scope.Commit();
+            }
+        }
+    }
+}
