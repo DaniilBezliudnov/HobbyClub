00431bc55bc416c657901d3e21685e998b4d6451
tmamchur <taras.mamchur@gmail.com>
2015-03-26 11:29:11 +0200

    FakeUserRepository created

---
+++ HobbyClub.Data/Fake/FakeUserRepository.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using HobbyClub.Data.Entities;
+
+namespace HobbyClub.Data.Fake
+{
+    class FakeUserRepository:IFakeRepository<User>
+    {
+        private Dictionary<int, User> fakeData;
+
+        public FakeUserRepository()
+        {
+
+        }
+        
+        public void Add(User entity)
+        {
+            
+            //throw new NotImplementedException();
+        }
+
+        public User Delete(User entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update(User entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IQueryable<User> FakeData
+        {
+            get
+            {
+                throw new NotImplementedException();
+            }
+            set
+            {
+                throw new NotImplementedException();
+            }
+        }
+    }
+}
