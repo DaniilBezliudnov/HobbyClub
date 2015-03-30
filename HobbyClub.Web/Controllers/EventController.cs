7f2fc0ba3ee74e6dfcef964f85e45f96a5e39b42
sofiiaTarashchuk <sofiia_Tarashchuk@epam.com>
2015-03-30 11:02:50 +0300

    event

--- HobbyClub.Web/Controllers/EventController.cs
+++ HobbyClub.Web/Controllers/EventController.cs
@@ -17,15 +17,18 @@ namespace HobbyClub.Web.Controllers
 
         public ActionResult GetAllEvents()
         {
-            var events = new List<EventsView>();
-            events.Add(new EventsView() { logoImg = "/Content/Images/events/mafia_logo.png", name = "Classic mafia", where = "Kyive, Zhylianska Street", when = "26/03/2015", available = "2 free sits" });
-            events.Add(new EventsView() { logoImg = "/Content/Images/events/salsa_logo.gif", name = "Salsa", where = "Kyive, Zhylianska Street", when = "26/03/2015", available = "2 free sits" });
-            events.Add(new EventsView() { logoImg = "/Content/Images/events/soccer_logo.png", name = "Soccer", where = "Kyive, Zhylianska Street", when = "26/03/2015", available = "2 free sits" });
-            events.Add(new EventsView() { logoImg = "/Content/Images/events/vegan_logo.jpg", name = "Vegan club", where = "Kyive, Zhylianska Street", when = "26/03/2015", available = "2 free sits" });
-            return View(new MainPageViewModel()
-            {
-                events = events
+
+            var events = new List<Events>();
+            events.Add(new Events() { logoImg = "/Content/Images/events/mafia_logo.png", name = "Classic mafia", where = "Kyive, Zhylianska Street", when = "26/03/2015", available = "2 free sits" });
+            events.Add(new Events() { logoImg = "/Content/Images/events/salsa_logo.gif", name = "Salsa", where = "Kyive, Zhylianska Street", when = "26/03/2015", available = "2 free sits" });
+            events.Add(new Events() { logoImg = "/Content/Images/events/soccer_logo.png", name = "Soccer", where = "Kyive, Zhylianska Street", when = "26/03/2015", available = "2 free sits" });
+            events.Add(new Events() { logoImg = "/Content/Images/events/vegan_logo.jpg", name = "Vegan club", where = "Kyive, Zhylianska Street", when = "26/03/2015", available = "2 free sits" });
+            
+            return View(new EventViewModel()
+            {                
+                events = events,              
             });
+            
         }
     }
 }
