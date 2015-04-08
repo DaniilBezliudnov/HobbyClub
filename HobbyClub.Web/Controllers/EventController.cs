15654ac6bc8567237fa707d9e2446c6353337bf0
sofiiaTarashchuk <sofiia_Tarashchuk@epam.com>
2015-04-08 13:50:37 +0300

    try to implement NinjectControlFactory

--- HobbyClub.Web/Controllers/EventController.cs
+++ HobbyClub.Web/Controllers/EventController.cs
@@ -4,6 +4,8 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using HobbyClub.Web.Models;
+using HobbyClub.Data.Abstract;
+using HobbyClub.Data.Entities;
 
 namespace HobbyClub.Web.Controllers
 {
@@ -14,21 +16,28 @@ namespace HobbyClub.Web.Controllers
         {
             return View();
         }
+        private IBaseRepository<Event> repository;
+
+        public EventController(IBaseRepository<Event> eventRepository)
+        {
+            this.repository = eventRepository;
+        }
 
         public ActionResult GetAllEvents()
         {
 
-            var events = new List<Events>();
-            events.Add(new Events() { logoImg = "/Content/Images/events/mafia_logo.png", name = "Classic mafia", where = "Kyive, Zhylianska Street", when = "26/03/2015", available = "2 free sits" });
-            events.Add(new Events() { logoImg = "/Content/Images/events/salsa_logo.gif", name = "Salsa", where = "Kyive, Zhylianska Street", when = "26/03/2015", available = "2 free sits" });
-            events.Add(new Events() { logoImg = "/Content/Images/events/soccer_logo.png", name = "Soccer", where = "Kyive, Zhylianska Street", when = "26/03/2015", available = "2 free sits" });
-            events.Add(new Events() { logoImg = "/Content/Images/events/vegan_logo.jpg", name = "Vegan club", where = "Kyive, Zhylianska Street", when = "26/03/2015", available = "2 free sits" });
-            
-            return View(new EventViewModel()
-            {                
-                events = events,              
-            });
-            
+             return View(repository.GetAll());
+             //var events = new List<Events>();
+             //events.Add(new Events() { logoImg = "/Content/Images/events/mafia_logo.png", name = "Classic mafia", where = "Kyive, Zhylianska Street", when = "26/03/2015", available = "2 free sits" });
+             //events.Add(new Events() { logoImg = "/Content/Images/events/salsa_logo.gif", name = "Salsa", where = "Kyive, Zhylianska Street", when = "26/03/2015", available = "2 free sits" });
+             //events.Add(new Events() { logoImg = "/Content/Images/events/soccer_logo.png", name = "Soccer", where = "Kyive, Zhylianska Street", when = "26/03/2015", available = "2 free sits" });
+             //events.Add(new Events() { logoImg = "/Content/Images/events/vegan_logo.jpg", name = "Vegan club", where = "Kyive, Zhylianska Street", when = "26/03/2015", available = "2 free sits" });
+
+             //return View(new EventViewModel()
+             //{
+             //    events = events,
+             //});
+
         }
     }
 }
---
+++ HobbyClub.Web/Infrastructure/NinjectControllerFactory.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Ninject;
+using System.Web.Mvc;
+using System.Web.Routing;
+using HobbyClub.Data.Concrete;
+using HobbyClub.Data.Abstract;
+using HobbyClub.Data.Entities;
+using HobbyClub.Data.Infrastructure.Context;
+
+namespace HobbyClub.Web.Infrastructure
+{
+     //наследуясь от фабрики используемой по умолчанию
+    //public class NinjectControllerFactory : DefaultControllerFactory
+    //{
+    //    private IKernel ninjectKernel;
+    //    public NinjectControllerFactory()
+    //    {
+    //        // создание контейнера
+    //        ninjectKernel = new StandardKernel();
+    //        AddBindings();
+    //    }
+    //    protected override IController GetControllerInstance(RequestContext requestContext, Type controllerType)
+    //    {
+    //        // получение объекта контроллера из контейнера
+    //        // используя его тип
+    //        return controllerType == null
+    //        ? null
+    //        : (IController)ninjectKernel.Get(controllerType);
+    //    }
+    //    private void AddBindings()
+    //    {
+    //        // конфигурирование контейнера
+    //        ninjectKernel.Bind<IBaseRepository<Event>>().To<GenericRepository<Event, HobbyClubIdentityDbContext>>().InSingletonScope();
+    //    }
+    //}
+    
+}
